d9676a561389f7c0b28def92a1853cf8e824029c
Kiran Hegde <kiransrivani@gmail.com>
2020-08-29 21:05:12 +0530

    22nd commit

--- js/script.js
+++ js/script.js
@@ -1,52 +1,54 @@
-$(document).ready(function() {
-	
-/* For the sticky navigation */
-    $('.js--section-about').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
+$(document).ready(function () {
+
+  /* For the sticky navigation */
+  $('.js--section-about').waypoint(function (direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  });
+
+
+  /* Scroll on buttons */
+  $('.js--scroll-to-about').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js--section-about').offset().top
+    }, 1000);
+  });
+
+
+  /* Navigation scroll */
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
         }
-    }, {
-      offset: '60px;'
+      }
     });
+  });
 
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
 
-    /* Scroll on buttons */
-    $('.js--scroll-to-about').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 1000);
-    });
+    nav.slideToggle(200);
 
-
-    /* Navigation scroll */
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-
-    /* Mobile navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-
-        nav.slideToggle(200);
-
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-    });
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
 });
