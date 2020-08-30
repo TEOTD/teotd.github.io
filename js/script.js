13f93bc64739ae57cb545e3bee2f657354c9dd06
Kiran Hegde <kiransrivani@gmail.com>
2020-08-30 22:33:03 +0530

    29th commit

--- js/script.js
+++ js/script.js
@@ -45,4 +45,20 @@ $(document).ready(function () {
       }
     });
   });
+
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('fa-bars')) {
+      icon.addClass('fa-times');
+      icon.removeClass('fa-bars');
+    } else {
+      icon.addClass('fa-bars');
+      icon.removeClass('fa-times');
+    }
+  });
 });
